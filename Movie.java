44d5730453b1b79beef112b5bf5636d6ee3d6821
MLDG <main.mldg@gmail.com>
2026-07-03 01:39:52 +0800

    feat movie class

---
+++ Movie.java
@@ -0,0 +1,64 @@
+import java.time.Year;
+
+public class Movie {
+    private String title;
+    private String director;
+    private int year;
+    private String description;
+
+    public Movie(String title, String director, int year, String description)
+    {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.description = description;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDirector()
+    {
+        return director;
+    }
+
+    public void setDirector(String director)
+    {
+        this.director = director;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String displayInfo()
+    {
+        return "Title: " + title +
+                "\nDirector: " + director +
+                "\nYear: " + year +
+                "\nDescription: " + description;
+    }
+}
