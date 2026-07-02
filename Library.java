67b459ee9d8d31aee8963199701c2abef5efe946
shenelle <shenellenono@gmail.com>
2026-07-02 22:16:36 +0800

    Profile and library initial draft

---
+++ Library.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Media> entries = new ArrayList<>();
+
+    public void addEntry(Media media)
+    {
+        entries.add(media);
+    }
+
+    public void removeEntry(Media media)
+    {
+        entries.remove(media);
+    }
+
+    public Media searchEntry(String title)
+    {
+        // int key = entries.indexOf() pano to help
+    }
+}
---
+++ Profile.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profile {
+    private String username;
+    private String displayName;
+    private String bio;
+    private Library library;
+    private List<Media> favorites = new ArrayList<>();
+    private List<Media> recentActivity = new ArrayList<>();
+
+    public Profile(String username, String displayName, String bio)
+    {
+        this.username = username;
+        this.displayName = displayName;
+        this.bio = bio;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getDisplayName()
+    {
+        return displayName;
+    }
+
+    public String getBio()
+    {
+        return bio;
+    }
+
+    public void addFavorite(Media media)
+    {
+        favorites.add(media);
+    }
+}
